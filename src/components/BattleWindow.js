4225520fc42b79bfaaf716ef0d669ec3311b9598
Blaine Anderson <BlaineA97@Gmail.com>
2018-04-08 18:11:55 -0700

    Started flexbox breakdown of BattleWindow

--- src/components/BattleWindow.js
+++ src/components/BattleWindow.js
@@ -5,13 +5,16 @@ class BattleWindow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      blank: null
+      playerHp: 10,
+      enemyHp: 10
     }
   }
   render() {
     return (
       <div id="BattleWindow">
-
+        <div id="LeftWindow">Player Window</div>
+        <div id="CenterWindow">Battle Calculator</div>
+        <div id="RightWindow">Enemy Window</div>
       </div>
     );
   }
