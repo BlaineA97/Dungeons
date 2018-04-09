fd3b58943008d025b1efbc02159387540a8fd053
Blaine Anderson <BlaineA97@Gmail.com>
2018-04-08 18:04:55 -0700

    Added Blank.js & Blank.css for future use. Created BattleWindow js & css as container

---
+++ src/components/BattleWindow.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './BattleWindow.css';
+
+class BattleWindow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blank: null
+    }
+  }
+  render() {
+    return (
+      <div id="BattleWindow">
+
+      </div>
+    );
+  }
+}
+
+export default BattleWindow;
